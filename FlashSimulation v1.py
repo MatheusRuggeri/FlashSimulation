6dc17a3e141aeba410b541004538e5781815558b
MatheusRuggeri <jomarumu@yahoo.com.br>
2020-07-09 13:46:41 -0300

    Heat Diffusion fixed
    Not a material only diffuses the heat through the same material (Disilicate <-> Disilicate)
    Do NOT diffuses through different materials (Disilicate <-/-> Air)

--- FlashSimulation v1.py
+++ FlashSimulation v1.py
@@ -27,8 +27,8 @@ INTERPOLATION = "None"
 '''INTERPOLATION = "bilinear"'''
 
 # ODE conditions
-DX=1
-DY=1
+DX=0.0001
+DY=0.0001
 alpha=5
 DT = (DX**2)/(2*alpha)
 
@@ -38,11 +38,26 @@ class circle:
         self.x = x
         self.y = y
         
-def heatDiffusion(TOld):
+def heatDiffusion(TOld, Compos):
     i = 1; j = 1;
-    Residuo = ((TOld[i+1,j]-2*TOld[i,j]+TOld[i-1,j])/DX**2)
-    Residuo += ((TOld[i,j+1]-2*TOld[i,j]+TOld[i,j-1])/DY**2)
-    Residuo *= DT
+    # Creates a new matrix where the value is True where the composition is the same as the center
+    diffusion = Compos[i,j] == Compos
+    
+    # In the 3rd line multiply by 1 since True + True is not 2, but True
+    ResiduoX =   (diffusion[i+1,j]) * TOld[i+1,j]
+    ResiduoX +=  (diffusion[i-1,j]) * TOld[i-1,j]
+    ResiduoX -=  (1*diffusion[i+1,j] + 1*diffusion[i-1,j]) * TOld[i,j]
+    ResiduoX /=  DX**2
+
+    ResiduoY =   (diffusion[i,j+1]) * TOld[i,j+1]
+    ResiduoY +=  (diffusion[i,j-1]) * TOld[i,j-1]
+    ResiduoY -=  (1*diffusion[i,j+1] + 1*diffusion[i,j-1]) * TOld[i,j]
+    ResiduoY /=  DY**2
+    
+    #Residuo = ((TOld[i+1,j]-2*TOld[i,j]+TOld[i-1,j])/DX**2)
+    #Residuo += ((TOld[i,j+1]-2*TOld[i,j]+TOld[i,j-1])/DY**2)
+    
+    Residuo = (ResiduoX + ResiduoY) * DT
     return Residuo
 
 # Create the Temperature and Composition matrix
@@ -51,16 +66,16 @@ composition = np.zeros((maxH,maxL))
 
 circle1 = circle(50,50)
 circle2 = circle(50,260)
-
+                
 # DOG BONE PART 1 - Draw the circles, in the dog bone
 for i in range(0, maxH):
     for j in range(0, maxL):
         if(((circle1.x - i)**2 + (circle1.y - j)**2 ) <= RADIUS**2 ):
             composition[i,j] = DISILICATE
-            temperature[i,j] = 100
+            temperature[i,j] = 1000
         if(((circle2.x - i)**2 + (circle2.y - j)**2 ) <= RADIUS**2 ):
             composition[i,j] = DISILICATE
-            temperature[i,j] = 100
+            temperature[i,j] = 1000
 
 # DOG BONE PART 2 - Draw the "body"
 for i in range(0, maxH):
@@ -70,17 +85,17 @@ for i in range(0, maxH):
         if(circle1.x - LENGTH <= i and i <= circle1.x + LENGTH):
             if (circle1.y <= j and j <= circle2.y):
                 composition[i,j] = DISILICATE
-                temperature[i,j] = 100
+                temperature[i,j] = 50
 
 # PLATINUM ELECTRODE
 for i in range(0, maxH):
     for j in range(0, maxL):
         if(((circle1.x - i)**2 + (circle1.y - j)**2 ) <= ELECTRODE_RADIUS**2 ):
             composition[i,j] = PLATINUM
-            temperature[i,j] = 300
+            temperature[i,j] = 20
         if(((circle2.x - i)**2 + (circle2.y - j)**2 ) <= ELECTRODE_RADIUS**2 ):
             composition[i,j] = PLATINUM
-            temperature[i,j] = 300
+            temperature[i,j] = 20
 
 # Heat diffusion
 print("Start simulation...")
@@ -92,13 +107,15 @@ while (True):
     TOld = temperature
     for i in range(2, maxH-1):
         for j in range(2, maxL-1):
-            Tvar = heatDiffusion(TOld[i-1:i+2,j-1:j+2])
+            Tvar = heatDiffusion(TOld[i-1:i+2,j-1:j+2], composition[i-1:i+2,j-1:j+2])
             temperature[i,j] = temperature[i,j] + Tvar
     if (nRun % 10 == 0):
         # Display the image
-        print("Img exporting...")
+        print("\nImg exporting ", end='')
         plt.imshow(temperature, interpolation=INTERPOLATION)
         plt.savefig('test'+str(nRun)+'.png', dpi=1000)
-        print("Img exported")
+        print("-> Img exported")
         #plt.colorbar()
         plt.show()
+    else:
+        print('.', end='')
