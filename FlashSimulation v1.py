78eedae99cf034471928c0828cd7a20fd9458f72
MatheusRuggeri <jomarumu@yahoo.com.br>
2020-07-09 10:36:54 -0300

    ODE for heat transfer and print the transfer overtime
    Add the ODE for heat transfer and print a image for every 10 steps in heat transfer

--- FlashSimulation v1.py
+++ FlashSimulation v1.py
@@ -26,6 +26,12 @@ LENGTH = 16
 INTERPOLATION = "None"
 '''INTERPOLATION = "bilinear"'''
 
+# ODE conditions
+DX=1
+DY=1
+alpha=5
+DT = (DX**2)/(2*alpha)
+
 # Obj to help with radius calculation
 class circle:
     def __init__(self, x, y):
@@ -44,9 +50,11 @@ for i in range(0, maxH):
     for j in range(0, maxL):
         if(((circle1.x - i)**2 + (circle1.y - j)**2 ) <= RADIUS**2 ):
             composition[i,j] = DISILICATE
+            temperature[i,j] = 100
         if(((circle2.x - i)**2 + (circle2.y - j)**2 ) <= RADIUS**2 ):
             composition[i,j] = DISILICATE
-            
+            temperature[i,j] = 100
+
 # DOG BONE PART 2 - Draw the "body"
 for i in range(0, maxH):
     for j in range(0, maxL):
@@ -55,17 +63,31 @@ for i in range(0, maxH):
         if(circle1.x - LENGTH <= i and i <= circle1.x + LENGTH):
             if (circle1.y <= j and j <= circle2.y):
                 composition[i,j] = DISILICATE
+                temperature[i,j] = 100
 
 # PLATINUM ELECTRODE
 for i in range(0, maxH):
     for j in range(0, maxL):
         if(((circle1.x - i)**2 + (circle1.y - j)**2 ) <= ELECTRODE_RADIUS**2 ):
             composition[i,j] = PLATINUM
+            temperature[i,j] = 300
         if(((circle2.x - i)**2 + (circle2.y - j)**2 ) <= ELECTRODE_RADIUS**2 ):
             composition[i,j] = PLATINUM
+            temperature[i,j] = 300
 
-
-# Display the image
-plt.imshow(composition, interpolation=INTERPOLATION)
-#plt.colorbar()
-plt.show()
+# Heat transfer
+nRun = 0
+while (True):
+    nRun += 1
+    ERR = 0
+    TOld = temperature
+    for i in range(2, maxH-1):
+        for j in range(2, maxL-1):
+            Residuo=(DT*((TOld[i+1,j]-2*TOld[i,j]+TOld[i-1,j])/DX**2 + (TOld[i,j+1]-2*TOld[i,j]+TOld[i,j-1])/DY**2) + TOld[i,j])-temperature[i,j]
+            ERR=ERR+abs(Residuo)
+            temperature[i,j]=temperature[i,j]+Residuo
+    if (nRun % 10 == 0):
+        # Display the image
+        plt.imshow(temperature, interpolation=INTERPOLATION)
+        #plt.colorbar()
+        plt.show()
